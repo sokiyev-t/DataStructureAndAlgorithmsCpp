474c2720a3b622fc42fc30f91fb228727eff4e7f
sokiyev-t <iron623@mail.ru>
2025-04-08 11:43:25 +0500

    Quck sort

--- Lecture3.cpp
+++ Lecture3.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 
-int factorial(int n)
+void quickSort(vector<int> &arr, int low, int high)
 {
-    if (n == 0)
+    if (low < high)
     {
-        return 1;
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
-    return n * factorial(n - 1);
 }
 
-int main()
-{
-    int n = 5;
-    std::cout << "Factorial of " << n << " is " << factorial(n) << std::endl;
-    return 0;
-}
+// int factorial(int n)
+// {
+//     if (n == 0)
+//     {
+//         return 1;
+//     }
+//     return n * factorial(n - 1);
+// }
+
+// int main()
+// {
+//     int n = 5;
+//     std::cout << "Factorial of " << n << " is " << factorial(n) << std::endl;
+//     return 0;
+// }
